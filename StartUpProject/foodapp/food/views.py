9faa6113dc9f0cf52b332f9e497202ff4bffca5a
lensherrggg <gr98522@gmail.com>
2020-03-22 11:42:40 +0800

    foodapp code format modified

--- StartUpProject/foodapp/food/views.py
+++ StartUpProject/foodapp/food/views.py
@@ -4,19 +4,19 @@ from .models import Item
 from django.template import loader
 
 # Create your views here.
-def index(request): 
-  item_list = Item.objects.all()
-  context = {
-    'item_list': item_list
-  }
-  return render(request, 'food/index.html', context)
+def index(request):
+    item_list = Item.objects.all()
+    context = {
+        'item_list': item_list
+    }
+    return render(request, 'food/index.html', context)
 
 def item(request):
-  return HttpResponse('<p><strong>This is item view</strong></p>')
+    return HttpResponse('<p><strong>This is item view</strong></p>')
 
 def detail(request, item_id):
-  item = Item.objects.get(pk=item_id)
-  context = {
-    'item': item,
-  }
-  return render(request, 'food/detail.html', context)
+    item = Item.objects.get(pk=item_id)
+    context = {
+        'item': item,
+    }
+    return render(request, 'food/detail.html', context)
