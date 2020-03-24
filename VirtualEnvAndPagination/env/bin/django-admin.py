3facb0e4487c7916c4431e59cc195b123ba65ce9
lensherrggg <gr98522@gmail.com>
2020-03-24 21:06:59 +0800

    VirtualEnv and Pagination and User permission finished

---
+++ VirtualEnvAndPagination/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/rui/Documents/Code/Learning/Django2MasterClass/VirtualEnvironment/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
