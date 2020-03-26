63f1c92b9bf2d8a3af1741440a779d987cff794a
lensherrggg <gr98522@gmail.com>
2020-03-26 21:58:43 +0800

    Admin Panel Customization finished

--- E-commerceSite/ecomsite/shop/admin.py
+++ E-commerceSite/ecomsite/shop/admin.py
@@ -3,5 +3,22 @@ from .models import Products, Order
 
 # Register your models here.
 
-admin.site.register(Products)
+admin.site.site_header = "E-commerce Site"
+admin.site.site_title = "ABC Shopping"
+admin.site.index_title = "Manage ABC Shopping"
+
+class ProductAdmin(admin.ModelAdmin):
+
+    def change_category_to_default(self, request, queryset):
+        queryset.update(category='default')
+
+    change_category_to_default.short_description = 'Default Category'
+
+    list_display = ('title', 'price', 'discount_price', 'category', 'description')
+    search_fields = ('title',)
+    actions = ('change_category_to_default',)
+    fields = ('title', 'price',)
+    list_editable = ('price', 'category', )
+
+admin.site.register(Products, ProductAdmin)
 admin.site.register(Order)
